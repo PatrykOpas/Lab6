2d26c9f94d70b806430f516ff7732bd3800b4f7d
Nevet one <patryk.opas.g@gmail.com>
2022-04-27 23:00:46 +0200

    samodzielne 2

--- src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/Controller.java
@@ -1,14 +1,48 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.FileChooser;
+import pl.lublin.wsei.java.cwiczenia.samodzielne.NoblistaMain;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public Label textFile;
+    public TableColumn colName;
+    public TableColumn colSurname;
+    public TableColumn colYear;
+    public TableColumn colCategory;
+    public TableColumn colMotivation;
+    public TableColumn colCountry;
+    public TableView tableNoblista;
+
+    NoblistaMain main = new NoblistaMain();
+    public void LoadFile(ActionEvent actionEvent) throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        FileChooser.ExtensionFilter xmlFilter = new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
+
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null){
+            textFile.setText(file.getAbsolutePath());
+            main.LoadFile(file.getAbsolutePath());
+        }else{
+            textFile.setText("Nie wybrano Pliku...");
+        }
+    }
+
+    void doTable(){
+
+    }
+
+
+    public void SaveFile(ActionEvent actionEvent) {
+
     }
 }
--- src/main/java/pl/lublin/wsei/java/cwiczenia/samodzielne/Noblista.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/samodzielne/Noblista.java
@@ -8,39 +8,66 @@ public class Noblista {
         DataRow = datarow;
         Split();
     }
-    void Split(){
+
+    public String getName(){
+        return (this.Imie + " " +this.Nazwisko);
+    }
+
+    ArrayList<String> ConvertData(String[] textToArray){
         ArrayList<String> columns = new ArrayList<>();
-
-        int TextStart = 0;
-        int TextEnd = 0;
-        char[] textToArray = this.DataRow.toCharArray();
-
+        String item = "";
         for(int i = 0; i < textToArray.length; i++){
-            TextEnd++;
-
-            /*
-           if(i == textToArray.length-1){
-                columns.add(new String());
+            if(textToArray[i].contains("\"") && (textToArray[i].toCharArray()[textToArray[i].toCharArray().length-1]) != '\"') {
+                if(item == ""){
+                    item += textToArray[i];
+                }
+                else if (item != ""){
+                    item += textToArray[i];
+                    columns.add(item);
+                    item = "";
+                }
             }
-            */
-
+            else if(textToArray[i].contains("\"") && (textToArray[i].toCharArray()[textToArray[i].toCharArray().length-1]) == '\"'){
+                item += textToArray[i];
+                columns.add(item);
+                item = "";
+            }
+            else if(!textToArray[i].contains("\"") && item != ""){
+                item += textToArray[i];
+            }
+            else{
+                columns.add(textToArray[i]);
+            }
         }
+        return columns;
+    }
+
+    void Split(){
+        String[] textToArray = this.DataRow.split(",");
+        ArrayList<String> columns = ConvertData(textToArray);
 
         for (int i = 0; i < columns.size(); i++){
             switch (i){
                 case 1:
-                    this.Imie = columns.get(i);
+                    this.Imie = columns.get(i).replace("\"","");
                     break;
                 case 2:
-                    this.Nazwisko = columns.get(i);
+                    this.Nazwisko = columns.get(i).replace("\"","");
                     break;
                 case 12:
-                    this.Rok = columns.get(i);
+                    this.Rok = columns.get(i).replace("\"","");
                     break;
                 case 13:
-                    this.Kategoria = columns.get(i);
+                    this.Kategoria = columns.get(i).replace("\"","");
+                    break;
+                case 16:
+                    this.Uzasadnienie = columns.get(i).replace("\"","");
+                    break;
+                case 19:
+                    this.Kraj = columns.get(i).replace("\"","");
                     break;
             }
+
         }
     }
 }
